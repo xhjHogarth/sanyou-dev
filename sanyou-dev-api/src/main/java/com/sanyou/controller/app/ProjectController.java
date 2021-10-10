4356af2c8070d1dfa5fd461652902af9cead6b63
一笔带过就是过 <useranan@163.com>
2021-10-10 13:20:31 +0800

    test commit

--- sanyou-dev-api/src/main/java/com/sanyou/controller/app/ProjectController.java
+++ sanyou-dev-api/src/main/java/com/sanyou/controller/app/ProjectController.java
@@ -1,10 +1,13 @@
 package com.sanyou.controller.app;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * User: asus
  * Date: 2021-10-10
  * Time: 12:56
  * Version:V1.0
  */
+@Controller
 public class ProjectController {
 }
